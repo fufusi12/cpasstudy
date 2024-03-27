fc4152d552ab7387719efa97a725a3c0e3861da4
JaeHyeon Park <fufusi12@naver.com>
2024-03-27 19:38:01 +0900

    first commit

---
+++ caps-1/main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int a, b;
+	scanf("%d %d", &a, &b);
+
+	printf("%lf \n", (double)a/(double)b);
+
+	return 0;
+}
+
+/*
+fdsfdfs
+fdssdfsdf
+fsdfdafsda
+*/
